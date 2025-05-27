57f74286da39328c4bb3ba3950f93432bc32e279
Pablo Salvi <salvipablo@gmail.com>
2025-05-27 01:34:47 -0300

    DB
    Resolve: T#022_partials

--- src/repository/tasks.repository.js
+++ src/repository/tasks.repository.js
@@ -17,15 +17,11 @@ import { ShowLog } from '../services/generals.service.js'
         time_repeat: task.timeRepeatTask
       }
 
-      console.log(taskDB)
-
       // Envio a guardar tarea a la base usando schema.
-      let taskSave = await TasksSchema.create({ taskDB })
-
-      console.log(taskSave)
+      let taskSave = await TasksSchema.create(taskDB)
 
       //Envio a guardar emails a base de datos.
-      //saveEmails(taskSave.id, task.mails)
+      saveEmails(taskSave.id, task.mails)
 
       // Si llego aca es porque el guardado finalizo correctamente, muestro un log en pantalla.
       ShowLog('The task was successfully stored in the database', 1)
@@ -42,10 +38,15 @@ import { ShowLog } from '../services/generals.service.js'
 
 const saveEmails = async (taskID, mails) => {
   try {
-    
-    // Recorro emails y los envio a guardar a la base de datos.
-    EmailForTaskSchema.create({  })
+    mails.forEach(async element => {
+      let newEmailSave = {
+        task_id: taskID,
+        mail: element
+      }
+
+      await EmailForTaskSchema.create(newEmailSave)
+    });
   } catch (error) {
-    
+    ShowLog(error.message, 2)
   }
 }
